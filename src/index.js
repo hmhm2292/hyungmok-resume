39ebbcee65a537d4da3b09b46322ca033c966c7a
hmhm2292 <hyungmok.kim92@gmail.com>
2022-11-14 19:04:52 +0900

    toggle dark-mode

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 var dark =
   window.matchMedia &&
   window.matchMedia("(prefers-color-scheme: dark)").matches;
-console.log({ dark });
 
 var element = document.body;
 var toggle = document.getElementById("toggle");
+toggle.onclick = toggleDarkMode;
 var toggleLabel = document.getElementById("toggleLabel");
 
 if (dark) {
-  console.log("dark");
   toggle.checked = true;
   toggleDarkMode();
 }
 
-toggle.onclick = toggleDarkMode;
-
 function toggleDarkMode() {
+  element.classList.toggle("dark-mode");
   if (toggle.checked) {
-    element.classList.add("dark-mode");
     toggleLabel.innerText = "Light";
   } else {
-    element.classList.remove("dark-mode");
     toggleLabel.innerText = "Dark";
   }
 }
